622b900bf92a614c05ec2373f4f0452b1f5ea82d
Naoki Nomoto <happygrind@gmail.com>
2017-01-19 02:41:04 +0900

    moved.but only one rule

--- Main.elm
+++ Main.elm
@@ -4,6 +4,10 @@ import Html exposing (Html, program, div, text)
 import Color exposing (..)
 import Element exposing (toHtml, show)
 import Collage exposing (..)
+import Random
+import Random.Color
+import Time exposing (..)
+import Array exposing (..)
 
 main : Program Never Model Msg
 main =
@@ -14,45 +18,119 @@ main =
         , subscriptions = subscriptions
         }
 
-type alias Cell =
-    { color: Color
-    , x: Int
-    , y: Int
-    }
+-- type Neighbourhood = Moore | Neumann
+
+-- type alias Rule =
+--     { range: Int
+--     , threshold: Int
+--     , count: Int
+--     , neighbourhood: Neighbourhood
+--     }
 
 type alias Model =
     { width: Int
     , height: Int
     , size: Int
-    , cells: List Cell
+    , cells: List (List Int)
+    , colors: List Color
     }
 
-type Msg = Update | Clear
+type Msg = Update | UpdateRandom (List (List Int))
 
 initModel : Model
 initModel =
-    { width = 300
-    , height = 300
-    , size = 50
-    , cells =
-          [ Cell red 0 0
-          , Cell blue 0 1
-          , Cell green 0 2
-          , Cell yellow 0 3
-          , Cell black 0 4
-          ]
+    { width = 500
+    , height = 500
+    , size = 10
+    , cells = 0 |> List.repeat 50 |> List.repeat 50
+    , colors = initColors
     }
 
 init : ( Model, Cmd Msg )
 init =
-    ( initModel, Cmd.none )
+    ( initModel, Random.generate UpdateRandom randomizeCells )
 
-update : Msg -> Model -> ( Model, Cmd a )
-update msg model = (model, Cmd.none)
+maxValue : Int
+maxValue = 14
+
+initColors : List Color
+initColors =
+  [ (255,0,0), (255,96,0), (255,191,0), (223,255,0), (128,255,0), (32,255,0), (0,255,64), (0,255,159), (0,255,255), (0,159,255), (0,64,255), (32,0,255), (127,0,255), (223,0,255) ]
+      |> List.map(\(r,g,b) -> Color.rgb r g b)
+
+getCellColor: List Color -> Int -> Color
+getCellColor colors cellValue =
+    let color = colors |> Array.fromList |> Array.get cellValue in
+    case color of
+        Just c -> c
+        Nothing -> Color.rgb 0 0 0
+
+getCoordValue : Int -> Int -> Int
+getCoordValue max v =
+    (v % max + max) % max
+
+getCellValue : Int -> Int -> Array (Array Int) -> Int
+getCellValue x y cellArray =
+    let
+        value = case (Array.get y cellArray) of
+                    Just a -> Array.get x a
+                    Nothing -> Nothing
+    in
+        case value of
+            Just v -> v
+            Nothing -> 0
+
+getNeighbors : Int -> Int -> Array (Array Int) -> List Int
+getNeighbors x y cellArray =
+    [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y), (x - 1, y - 1), (x - 1, y + 1), (x + 1, y - 1), (x + 1, y + 1)]
+        |> List.map(\(x, y) -> ((getCoordValue 50 x), (getCoordValue 50 y)))
+        |> List.map(\(x, y) -> getCellValue x y cellArray)
+
+getNextCellValue : Int -> Int -> Int -> Array (Array Int) -> Int
+getNextCellValue x y currentValue cellArray =
+    let
+        nextValue = (currentValue + 1) % maxValue
+        neighbors = getNeighbors x y cellArray
+    in
+        if List.member nextValue neighbors then
+            nextValue
+        else
+            currentValue
+
+updateCells : List (List Int) -> List (List Int)
+updateCells cells =
+    let cellArray =
+            cells
+                |> List.map(\row -> row |> Array.fromList)
+                |> Array.fromList
+    in
+        cells
+            |> List.indexedMap(\y row -> row
+                              |> List.indexedMap(\x cell -> getNextCellValue x y cell cellArray))
+
+randomizeCells : Random.Generator (List (List Int))
+randomizeCells =
+    Random.list 50 (Random.int 0 15)
+        |> Random.list 50
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UpdateRandom cells -> (Model model.width model.height model.size cells model.colors, Cmd.none)
+        Update -> (Model model.width model.height model.size (updateCells model.cells) model.colors, Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Time.every (Time.millisecond * 50) (always Update)
+
+formLine : Float -> Int -> Float -> List Color -> List Int -> List Form
+formLine size width top colors line =
+    let
+        offset = ((toFloat width) / 2) - (size / 2)
+    in
+        List.indexedMap(\x cell -> rect size size
+                       |> filled (getCellColor colors cell)
+                       |> move ((toFloat x) * size - offset, top)) line
 
 view : Model -> Html Msg
 view model =
@@ -62,11 +140,8 @@ view model =
         l = toFloat w / 2
         t = toFloat h / 2
         s = toFloat model.size
+        yoffset = t - (s / 2)
         cells = model.cells
-        c = collage w h [
-             group(List.map(\cell -> rect s s
-                           |> filled cell.color
-                           |> move ((toFloat cell.x) * s - l, (toFloat cell.y) * s - t)) cells)
-            ]
+        colors = model.colors
     in
-        toHtml c
+        toHtml (collage w h [ group(List.concat (List.indexedMap(\y line -> formLine s w ((toFloat y) * s - yoffset) colors line) cells)) ])
